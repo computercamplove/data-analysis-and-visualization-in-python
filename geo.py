9ca7cf5ca48bae414332ae6e9e7c9aad8bf0cfc2
zhamilya <jozefineme@gmail.com>
2021-01-01 15:48:31 +0100

    new script

---
+++ geo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3.8
+# coding=utf-8
+import pandas as pd
+import geopandas
+import matplotlib.pyplot as plt
+import contextily
+import sklearn.cluster
+import numpy as np
+import os
+import sys
+# muzeze pridat vlastni knihovny
+
+def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
+    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
+    df = df.dropna()
+
+    return df
+
+
+def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
+             show_figure: bool = False):
+    """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
+
+
+def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
+                 show_figure: bool = False):
+    """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
+
+
+
+if __name__ == "__main__":
+    # zde muzete delat libovolne modifikace
+    # crs="EPSG:5514"
+    file = sys.argv[0] 
+    path_project = os.path.abspath(file+"/..")
+    data = pd.read_pickle(path_project + "/"+ 'accidents.pkl.gz', compression='gzip')
+    print(data.tail())
+
+    gdf = make_geo(data)
+    print(gdf.tail())
+    plot_geo(gdf, "geo1.png", True)
+    plot_cluster(gdf, "geo2.png", True)
+
